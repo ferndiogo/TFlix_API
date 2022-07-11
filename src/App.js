b343d8fc7cc4b1f6dee566ad982e257f41739a8d
Fernando Fuzeiro <aluno22111@ipt.pt>
2022-07-11 20:12:27 +0100

    Leitura de dados dos utlizadores

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Modal, ModalBody, ModalFooter, ModalHeader} from 'reactstrap';
+import logoUsers from './assets/user.jpg';
+
 function App() {
+
+  const baseUrl = "https://localhost:44365/API/UtilizadoresAPI";
+
+  const [data, setData] = useState([]);
+
+  const pedidoGet = async() => {
+    await axios.get(baseUrl)
+    .then(response => {
+      setData(response.data);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+
+  useEffect(() => {
+    pedidoGet();
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <br />
+      <h3>Criação de Utilizadores</h3>
+      <header>
+        <img src={logoUsers} alt='Users'/>
+        <button className="btn btn-success">Criar Utilizador</button>
       </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>NIF</th>
+            <th>Morada</th>
+            <th>País</th>
+            <th>Código Postal</th>
+            <th>Sexo</th>
+            <th>Data de Nascimento</th>
+            <th>Subscrição</th>
+            <th>Aluguer</th>
+            <th>Função</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(utilizador =>(
+            <tr key={utilizador.id}>
+              <td>{utilizador.id}</td>
+              <td>{utilizador.nome}</td>
+              <td>{utilizador.email}</td>
+              <td>{utilizador.nif}</td>
+              <td>{utilizador.morada}</td>
+              <td>{utilizador.pais}</td>
+              <td>{utilizador.codPostal}</td>
+              <td>{utilizador.sexo}</td>
+              <td>{utilizador.dataNasc}</td>
+              <td>{utilizador.subscricoes}</td>
+              <td>{utilizador.aluguer}</td>
+              <td>{utilizador.userF}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button> {"   "}
+                <button className="btn btn-danger">Apagar</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
